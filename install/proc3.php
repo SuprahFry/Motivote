7149d75e6f9cbfc4136a5ee0318f77454aec3876
Aaron Croissette <sysfly@gmail.com>
2015-02-02 21:02:52 -0500

    Fixed admin pass creation

--- install/proc3.php
+++ install/proc3.php
@@ -3,7 +3,7 @@ if (!empty($_POST['server_name']) && !empty($_POST['incentive_tactic'])
 		&& !empty($_POST['admin_pass'])) {
 	mv_update_setting('server_name', $_POST['server_name']);
 	mv_update_setting('incentive_tactic', $_POST['incentive_tactic']);
-	mv_update_setting('admin_pass', $_POST['admin_pass']);
+	mv_update_setting('admin_pass', md5($_POST['admin_pass']));
 	mv_update_setting('security_hash', substr(md5(rand()), 0, 8));
 	mv_update_setting('datakey', substr(md5(rand()), 0, 8));
 	$_SESSION['admin'] = $_POST['admin_pass'];
